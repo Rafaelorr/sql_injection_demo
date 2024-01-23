9ce2e32211f3a4996cba6cec829be1c3718250c2
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-01-23 17:28:21 +0100

    voeg sample data aan de database

--- database_setup.py
+++ database_setup.py
@@ -1,9 +1,21 @@
 import sqlite3
 
-con = sqlite3.connect('database.db')
+con = sqlite3.connect('drop.db')
 cur = con.cursor()
 
-cur.execute('CREATE TABLE users VALUES(id INTEGER AUTO INCREMENT PRIMARY KEY, naam TEXT, wachtwoord TEXT)')
+cur.execute('DROP TABLE users')
+con.commit()
+
+cur.execute('CREATE TABLE users (id INTEGER AUTO INCREMENT PRIMARY KEY, naam TEXT, wachtwoord TEXT)')
+con.commit()
+
+cur.execute('INSERT into users VALUES(1,"admin","root")')
+con.commit()
+
+cur.execute('INSERT into users VALUES(2,"test","test")')
+con.commit()
+
+cur.execute('INSERT into users VALUEs(3,"jeff","password123")')
 con.commit()
 
 cur.close()
