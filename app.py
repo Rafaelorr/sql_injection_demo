0a5d5662f5061d209e86f667962b00e80ee337e5
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-01-27 13:57:04 +0100

    voeg comments toe aan app.py

--- app.py
+++ app.py
@@ -6,13 +6,17 @@ app = Flask(__name__)
 @app.route("/create_acount")
 def home():
   if request.method == "POST":
+    # neem de data van het account create form
     naam = request.form.get('gebruikersnaam')
     wachtwoord = request.form.get('wachtwoord')
+
     # voeg user toe aan database
     con = sqlite3.connect('drop.db')
     cur = con.cursor()
     cur.executescript("INSERT into users (naam,wachtwoord) VALUES(" + naam + "," + wachtwoord + ")")
     con.commit()
+
+    # sql injection dection
     try:
       cur.execute('SELECT * FROM users')
     except:
