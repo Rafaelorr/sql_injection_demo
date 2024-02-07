b766250d7e3cbff744b79e952a8c25283a6ff778
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-02-07 20:33:06 +0100

    voeg login bypass payload toe aan sql_injection_test

--- sql_injection_test.py
+++ sql_injection_test.py
@@ -3,10 +3,11 @@ import sqlite3
 con = sqlite3.connect('dev.db')
 cursor = con.cursor()
 
-command1 = """CREATE TABLE IF NOT EXISTS
-gebruikers(id INTEGER PRIMARY KEY, naam TEXT, wachtwoord TEXT)"""
-
-cursor.execute(command1)
+# cursor.execute('DROP table gebruikers')
+# # create gebruikers table
+# command1 = """CREATE TABLE IF NOT EXISTS
+# gebruikers(id INTEGER, naam TEXT, wachtwoord TEXT)"""
+# cursor.execute(command1)
 
 # voeg sampel data toe aan database
 data = (1,'test_1','test_1')
@@ -19,20 +20,30 @@ data = (4,'jefke','patato')
 cursor.execute("INSERT INTO gebruikers VALUES(?, ?, ?)", data)
 con.commit()
 
-cursor.execute("SELECT * FROM gebruikers")
+# login bypass payload
+print('login bypass payload:')
+login_bypass_aanval = "'admin'-- "
+cursor.execute("SELECT * from gebruikers WHERE naam="+ login_bypass_aanval+" AND wachtwoord='dd'")
 results = cursor.fetchall()
 print(results)
-aanval = "'paswoord'); DROP TABLE gebruikers; --"
-data = (4548888888, 'test_2',aanval)
-cursor.execute("INSERT INTO gebruikers VALUES(?, ?, ?)", data)
-con.commit()
-# cursor.executescript("INSERT INTO gebruikers VALUES(727,'jefke', " + aanval + "}")
+results = results[0]
+print(results)
+results = results[1]
+print(results)
+
+
+# # drop table payload
+# cursor.execute("SELECT * FROM gebruikers")
+# results = cursor.fetchall()
+# print(results)
+# drop_aanval = "'paswoord'); DROP TABLE gebruikers; --"
+# data = (4548888888, 'test_2',drop_aanval)
+# cursor.execute("INSERT INTO gebruikers VALUES(?, ?, ?)", data)
 # con.commit()
+# cursor.executescript("INSERT INTO gebruikers VALUES(727,'jefke', " + drop_aanval + "}")
+# con.commit()
+# cursor.execute("SELECT * FROM gebruikers")
+# results = cursor.fetchall()
+# print(results)
 
-cursor.execute("SELECT * FROM gebruikers")
-results = cursor.fetchall()
-print(results)
-
-results = cursor.fetchall()
-print(results)
 con.close()
