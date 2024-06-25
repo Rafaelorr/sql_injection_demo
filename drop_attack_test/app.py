4c19049012c7cc300f24c379dd8e44402bbccf01
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-06-25 18:36:09 +0200

    voeg better comment syntax toe aan app.py voor drop_attack_test

--- drop_attack_test/app.py
+++ drop_attack_test/app.py
@@ -6,27 +6,28 @@ app = Flask(__name__)
 @app.route("/create_acount",methods=["GET","POST"])
 def home():
   if request.method == "POST":
-    # neem de data van het account create form
+    #* neem de data van het account create form
     naam:str = request.form.get('gebruikersnaam')
     wachtwoord:str = request.form.get('wachtwoord')
 
-    # voeg user toe aan database
+    #* voeg user toe aan database
     con:sqlite3.Connection = sqlite3.connect('database.db')
     cur:sqlite3.Cursor = con.cursor()
+    # ! error op de volgende lijn als je sql injection probeert te doen
     cur.executescript("INSERT into users (naam,wachtwoord) VALUES(" + naam + "," + wachtwoord + ")")
     con.commit()
 
     # sql injection dection
-      # faal dection
+    #* faal dection
     try:
       cur.execute('SELECT * FROM users')
       return render_template('faal.html')
       
-      # succes dection
+    #* succes dection
     except sqlite3.OperationalError:
       return render_template("succes.html")
     
-      # fail save
+    #* fail save
     except:
       return render_template("home.html")
 
@@ -36,6 +37,6 @@ def home():
 def hints():
   return render_template("hints.html")
 
-# maak debug False
+# TODO: maak debug False
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
