65a0243980febcdeeda74c02ed9cf76057c7a0ae
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-07-07 16:44:01 +0200

    bug fix in app.py voor de login bypass test

--- login_bypass_test/app.py
+++ login_bypass_test/app.py
@@ -6,24 +6,21 @@ app = Flask(__name__)
 @app.route("/login",methods=["GET","POST"])
 def home():
   if request.method == "POST":
-    # neem de data van het login form
-    naam:str = request.form.get('gebruikersnaam')
-    wachtwoord:str = request.form.get('wachtwoord')
+    naam :str = request.form.get('gebruikersnaam')
+    wachtwoord :str = request.form.get('wachtwoord')
 
-    con:sqlite3.Connection = sqlite3.connect('database.db')
-    cur:sqlite3.Cursor = con.cursor()
+    con :sqlite3.Connection = sqlite3.connect('database.db')
+    cur :sqlite3.Cursor = con.cursor()
 
-    # select sql statement
-    cur.execute("SELECT count(*) FROM users WHERE naam="+ naam +" AND wachtwoord="+ wachtwoord+"")
-    database_wachtwoord = cur.fetchone()
-    database_wachtwoord = database_wachtwoord[0]
+    cur.execute(f"SELECT COUNT(*) FROM users WHERE naam='{naam}' AND wachtwoord='{wachtwoord}'")
+    user_count = cur.fetchone()
+    user_count = user_count[0]
 
-    if database_wachtwoord >= 1:
+    if user_count >= 1:
       return render_template("succes.html")
-    elif database_wachtwoord < 0:
+    elif user_count == 0:
       return render_template("faal.html")
 
-    return render_template("login.html")
   return render_template("login.html")
 
 @app.route("/")
