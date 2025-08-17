809b31bf09170c9749f79b8f478e404d33b5fd97
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-08-17 16:06:31 +0200

    bewaar de comments van de xss_test in een database

--- xss_test/app.py
+++ xss_test/app.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template, request
+from functies import *
 
 app = Flask(__name__)
 
-comments = []
+comments = read_comments_from_db("database.db")
 
 @app.route('/')
 def blog_post():
+    comments = read_comments_from_db("database.db")
+
     return render_template('index.html', comments=comments)
 
 @app.route('/add_comment', methods=['POST'])
 def add_comment():
     comment = request.form['comment']
-    comments.append(comment)
+    add_comments_to_db("database.db",[comment])
+
+    comments = read_comments_from_db("database.db")
+
     return render_template('index.html', comments=comments)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True)
